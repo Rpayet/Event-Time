11f06b29521dce015bc292b9a0f93236ad048c3a
Rpayet <robinpayet@gmail.com>
2023-03-18 19:29:39 +0100

    Fictures setup - OMG is this working ?

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Event;
+use DateTime;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,8 +12,17 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $event = new Event();
-        $manager->persist($event);
+        for ($i = 0; $i > 8; $i++) {
+            $event = new Event();
+            $event->setName('Concert '. $i);
+            $event->setDescription('Lorem Poue Pouet');
+            $event->setPrice(rand(10,50));
+            $event->setPublishedAt(new DateTimeImmutable('now'));
+            $event->setDateStart( new DateTime('+ 1 day'));
+            $event->setDateEnd(new DateTime('+ 3 days'));
+            $event->setPicture('picture'.$i.'.jpg');
+            $manager->persist($event);
+        }
 
         $manager->flush();
     }
