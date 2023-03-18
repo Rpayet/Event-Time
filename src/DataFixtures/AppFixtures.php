3632e1861466a9c1866b34fc1b4d85f0ba1289fa
Rpayet <robinpayet@gmail.com>
2023-03-18 22:48:49 +0100

    Creation d'un provider perso

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,14 +7,19 @@ use DateTime;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Provider\ConcertGroup;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $faker = Factory::create('fr_FR');
+        $faker->addProvider(new ConcertGroup($faker));
+
         for ($i = 1; $i < 9; $i++) {
             $event = new Event();
-            $event->setName('Concert '. $i);
+            $event->setName($faker->groupName());
             $event->setDescription('Lorem Poue Pouet');
             $event->setPrice(rand(10,50));
             $event->setPublishedAt(new DateTimeImmutable('now'));
