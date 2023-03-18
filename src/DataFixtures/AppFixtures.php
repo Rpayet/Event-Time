5c2a55a0c088ceeeb3648b86c2c6f6d568c875c6
Rpayet <robinpayet@gmail.com>
2023-03-18 19:21:19 +0100

    Fixtures - Init

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Event;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $event = new Event();
+        $manager->persist($event);
+
+        $manager->flush();
+    }
+}
