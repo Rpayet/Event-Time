a31e595e09b96719105ffff635071a42b633cb08
Rpayet <robinpayet@gmail.com>
2023-03-19 09:32:42 +0100

    EventController - Function create

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Form\EventType;
 use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,15 +17,18 @@ class EventController extends AbstractController
     #[Route('/event', name: 'app_event')]
     public function index(Request $request, EventRepository $repository, ValidatorInterface $validator): Response
     {
-                
+        
         return $this->render('event/index.html.twig', [
             'events' => $repository->findAll(),
         ]);
     }
 
     #[Route('/event/create', name: 'app_event_create')]
-    public function create(): Response
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $event = new Event();
+        $form = $this->createForm(EventType::class, $event);
+        
         return $this->render('event/create.html.twig');
     }
 
--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,9 @@ class EventType extends AbstractType
         $builder
             ->add('name')
             ->add('description')
-            ->add('price')
+            ->add('price', MoneyType::class, [
+                'divisor' => 100,
+            ])
             ->add('publishedAt')
             ->add('dateStart')
             ->add('dateEnd')
